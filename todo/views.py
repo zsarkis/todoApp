7261bde596b894c799791c970febb8e13347d267
Zach Sarkis <zach.sarkis@cognex.com>
2020-01-13 13:43:26 -0700

    Add Clear and Delete

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from .models import TodoItem
 
 # Create your views here.
 
 def todoView(request):
-    return render(request, 'todo.html')
+    allTodoItems = TodoItem.objects.all()
+    return render(request, 'todo.html',
+                  {'all_items': allTodoItems})
+
+def addTodo(request):
+    content = request.POST['content']
+    new_item = TodoItem(content= content)
+    new_item.save()
+    return HttpResponseRedirect('/todo/')
+
+def deleteTodo(request, todo_id):
+    item = TodoItem.objects.get(id= todo_id)
+    item.delete()
+    return HttpResponseRedirect('/todo/')
+
+def clear(request):
+    for item in TodoItem.objects.all():
+        item.delete()
+    return HttpResponseRedirect('/todo/')
